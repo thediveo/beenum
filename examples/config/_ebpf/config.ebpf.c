733789031c5ed2a53d8c84e12c55822b646932c0
thediveo <thediveo@gmx.eu>
2026-03-27 17:36:38 +0000

    wip
    Signed-off-by: thediveo <thediveo@gmx.eu>

--- examples/config/_ebpf/config.ebpf.c
+++ examples/config/_ebpf/config.ebpf.c
@@ -7,7 +7,15 @@
 
 char __license[] SEC("license") = "MIT";
 
+// For background information, please refer to:
+// https://github.com/cilium/ebpf/discussions/1567
+
 typedef enum {
     MAX_BEES = 42,
-    DEFAULT_FOOBAR = (1 << 48),
-} configuration;
+    DEFAULT_FOOBAR = (1ULL << 48),
+} defines;
+
+// Coerce the compiler to consider the enum as being used so that it emits the
+// desired enum type information into the ELF.
+static __attribute__((used)) defines __defines;
+
--- examples/config/generate.go
+++ examples/config/generate.go
@@ -1,3 +1,3 @@
-//go:generate bpf2go -go-package config configuration _ebpf/config.ebpf.c -- -I../../_headers -I../../_headers/libbpf
+//go:generate bpf2go -go-package config config _ebpf/config.ebpf.c -- -I../../_headers -I../../_headers/libbpf
 
 package config
